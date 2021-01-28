618046380bf588f997b17bc1e15e53f1abb30a89
Vaibhav Gupta <vbthegupta@gmail.com>
2021-01-28 17:40:43 -0500

    Final Blob Commit

--- blobs.py
+++ blobs.py
@@ -13,18 +13,24 @@ centers = 4, n_features=2, cluster_std=1.6, random_state=111)
 
 ## Import Kmeans from sklearn
 from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters=4)
+kmeans = KMeans(n_clusters=4, random_state=222)
 kmeans.fit(data[0])
 plt.scatter(data[0][:,0], data[0][:,1], c = data[1], cmap = "viridis")
 
 # %%
 features = data[0]
 
-
+## plot the clusters with their entroids
 def plot_centroid_clusters():
     clusters = kmeans.cluster_centers_
     y_kmeans = kmeans.fit_predict(features)
-    print(y_kmeans)
+    # Array of Kmeans to DataFrame
+    df_means = pd.DataFrame(y_kmeans, columns = ['Kmeans'])
+    print(len(df_means))
+    # DataFrame of the features and the Kmeans
+    df = pd.DataFrame(features, df_means.Kmeans)
+    print(df)
+    #print(y_kmeans)
     plt.scatter(features[y_kmeans == 0,0], features[y_kmeans == 0,1], s = 20, color = "blue")
     plt.scatter(features[y_kmeans == 1,0], features[y_kmeans == 1,1], s = 20, color = "green")
     plt.scatter(features[y_kmeans == 2,0], features[y_kmeans == 2,1], s = 20, color = "yellow")
